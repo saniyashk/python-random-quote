f7fea530d4f1fdc4af04a35e11aaecd00d92fd29
Saniya <saniya682shk@gmail.com>
2020-09-29 21:28:48 +0530

    final changes

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,14 @@ import random
 def main():
     #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt","a+")
   quotes = f.readlines()
+  x = ["old is gold","procastination is the thief of time"]
+  quotes.append(x)
   f.close()
-  last = 13
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+  #last = 13
+  #rnd = random.randint(0,last)
+  print(quotes)
   
 
 if __name__== "__main__":
